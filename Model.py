658fba59f2c4c764fabf080a500f80c76428e277
Ahmedazim7804 <54241827+Ahmedazim7804@users.noreply.github.com>
2020-08-06 10:36:00 +0530

    Add files via upload

--- Model.py
+++ Model.py
@@ -1,61 +1,45 @@
 import numpy as np
-import pdb,copy,sys,copy,time
+import copy
 
 class Sudoku():
-    def __init__(self):
-        self.array = np.array([[0,0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0,0]
-                         ],dtype=int)
-        with open("C:\\Users\\m\\asd.txt",'r') as file:
-            file = file.read()
-            n = 0
-            for i in range(9):
-                for j in range(9):
-                    self.array[i][j] = int(file[n])
-                    n += 1
+    def __init__(self,array):
+        self.array = array
 
-
+    def sd(self):
         list = np.where(self.array != 0)
         self.list = [(list[0][i],list[1][i]) for i in range(len(list[0]))]
         self.grid_dict = self.grid_assign()
         self.grid_dict_2 = {0 : {(i,j) : self.array[i][j] for i in range(3) for j in range(3)},1 : {(i,j) : self.array[i][j] for i in range(3) for j in range(3,6)},2 : {(i,j) : self.array[i][j] for i in range(3) for j in range(6,9)},3 : {(i,j) : self.array[i][j] for i in range(3,6) for j in range(3)},4 : {(i,j) : self.array[i][j] for i in range(3,6) for j in range(3,6)},5 : {(i,j) : self.array[i][j] for i in range(3,6) for j in range(6,9)},6 : {(i,j) : self.array[i][j] for i in range(6,9) for j in range(3)},7 : {(i,j) : self.array[i][j] for i in range(6,9) for j in range(3,6)},8 : {(i,j) : self.array[i][j] for i in range(6,9) for j in range(6,9)}}
-        self.check_all()
+        b = self.check_all()
+        if type(b) == str:
+            return b
         self.values = {}
         list_2 = np.where(self.array == 0)
         self.list_2 = [(list_2[0][i],list_2[1][i]) for i in range(len(list_2[0]))]
         self.possible_value()
-        start = time.time()
         self.sol()
-        print("--- %s seconds ---" % (time.time() - start))
-        print(self.array)
+        return self.array
+
     def check_all(self):
         for i in self.list:
-            if self.conditions(i):
-                continue
-            else:
-                print("Same Elements")
-                return
+            a = self.conditions(i)
+            if a == True:continue
+            else:return a
+
     def conditions(self,i):
         #FOR ROWS
         for j in range(9):
             if j == i[1]:
                 continue
             elif self.array[i[0],i[1]] == self.array[i[0],j]:
-                return False
+                return f'Row {i[0]+1}'
 
         #FOR COLUMN
         for j in range(9):
             if j == i[0]:
                 continue
             elif self.array[i[0],i[1]] == self.array[j,i[1]]:
-                return False
+                return f'Column {i[1]+1}'
 
         #FOR GRID
         grid = self.grid_dict_2[self.grid_dict[i]]
@@ -63,7 +47,7 @@ class Sudoku():
             if j == i:
                 continue
             elif grid[j] == self.array[i[0],i[1]]:
-                return False
+                return f'Grid {self.grid_dict[i]+1}'
         return True
     def grid_assign(self):
         l = np.where(self.array == self.array)
@@ -71,21 +55,21 @@ class Sudoku():
         grid = {}
         for i in l:
             if 0 <= i[0] <= 2:
-                if 0<= i[1] <= 2:
+                if 0 <= i[1] <= 2:
                     grid[i] = 0
                 elif 3 <= i[1] <= 5:
                     grid[i] = 1
                 elif 6 <= i[1] <= 8:
                     grid[i] = 2
             elif 3 <= i[0] <= 5:
-                if 0<= i[1] <= 2:
+                if 0 <= i[1] <= 2:
                     grid[i] = 3
                 elif 3 <= i[1] <= 5:
                     grid[i] = 4
                 elif 6 <= i[1] <= 8:
                     grid[i] = 5
             elif 6 <= i[0] <= 8:
-                if 0<= i[1] <= 2:
+                if 0 <= i[1] <= 2:
                     grid[i] = 6
                 elif 3 <= i[1] <= 5:
                     grid[i] = 7
@@ -102,7 +86,7 @@ class Sudoku():
         for j in self.values[s]:
             self.array[r][c] = j
             self.grid_dict_2[self.grid_dict[s]][s] = j
-            if self.conditions(s):
+            if self.conditions(s) == True:
                 self.list_2.remove(s)
                 if self.sol():
                     return True
@@ -120,5 +104,3 @@ class Sudoku():
                     self.values[i].remove(j)
             else:
                 self.array[i[0]][i[1]] = 0
-
-a = Sudoku()
